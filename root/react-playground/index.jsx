6acc8122ccc96cb93f58a35e2d905875a883428e
April <April@Rain>
2020-02-16 14:36:45 +0000

    lots

--- root/react-playground/index.jsx
+++ root/react-playground/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable react/prop-types */
 /* eslint-disable consistent-return */
 
@@ -6,60 +7,78 @@ import "./index.css";
 
 const defaultState = {
   money: 0,
-  farmer: 0,
+  workers: {
+    farmer: 0,
+    miner: 0,
+  },
+};
+
+const defaultWorkers = {
+  farmer: {
+    cost: 2,
+    mps: 2,
+  },
+  miner: {
+    cost: 200,
+    mps: 20,
+  },
+};
+
+const calculateMps = (workers) => Object.keys(workers).reduce(
+  (acc, worker) => acc + (workers[worker] * defaultWorkers[worker].mps), 0,
+);
+
+
+const stateReducer = (currentState, action) => {
+  const { money, workers } = currentState;
+  const { payload } = action;
+  switch (action.type) {
+    case "IncrementMoney":
+      return {
+        ...currentState,
+        money: payload + money + calculateMps(workers),
+      };
+
+    case "DecrementMoney":
+      return {
+        ...currentState,
+        money: money - payload,
+      };
+
+    case "HireWorker":
+      if (money >= defaultWorkers[payload].cost) {
+        return {
+          ...currentState,
+          workers: {
+            ...workers,
+            [payload]: workers[payload] + 1,
+          },
+          money: money - defaultWorkers[payload].cost,
+        };
+      }
+      console.log(`Not enough funds: ${money} vs ${defaultWorkers[payload].cost}`);
+      return currentState;
+    default:
+      return currentState;
+  }
 };
 
 const App = () => {
   const [tick, setTick] = useState(0);
   const [toggle, setToggle] = useState(true);
 
-  const [state, dispatch] = useReducer((currentState, action) => {
-    const { money, farmer } = currentState;
+  const [state, dispatch] = useReducer(stateReducer, defaultState);
 
-    switch (action.type) {
-      case "IncrementMoney":
-        return {
-          ...currentState,
-          money: money + farmer + action.amount,
-        };
-
-      case "DecrementMoney":
-        return {
-          ...currentState,
-          money: money - action.amount,
-        };
-
-      case "HireWorker":
-        return {
-          ...currentState,
-          [action.worker]: currentState[action.worker] + 1,
-          money: money - action.cost,
-        };
-
-      default:
-        return currentState;
-    }
-  }, defaultState);
-
-  const { money, farmer } = state;
-
-  const handleHire = () => {
-    if (money >= 2) {
-      dispatch({ type: "HireWorker", worker: "farmer", cost: 2 });
-    }
-  };
+  const { money, workers: { farmer, miner } } = state;
 
   const handleToggle = () => {
     setToggle(!toggle);
   };
 
   useEffect(() => {
-    const handleTick = () => {
-      setTick((prevTick) => prevTick + 1);
-    };
     if (toggle) {
       const timer = setTimeout(() => {
-        handleTick();
+        setTick((prevTick) => prevTick + 1);
       }, 1000);
 
       return () => {
@@ -69,28 +88,40 @@ const App = () => {
   }, [tick, toggle]);
 
   useEffect(() => {
-    dispatch({ type: "IncrementMoney", amount: 1 });
-  }, [tick]);
+    if (toggle) {
+      dispatch({ type: "IncrementMoney", payload: 1 });
+    }
+  }, [tick, toggle]);
 
   return (
     <div>
       <div className="counterDisplay">{`${money}`}</div>
-      <CustomButton text="Increment" onClick={() => dispatch({ type: "IncrementMoney", amount: 1 })} />
-      <CustomButton text="Decrement" onClick={() => dispatch({ type: "DecrementMoney", amount: 1 })} />
-      <CustomButton text={`Hire Farmer (${farmer})`} onClick={handleHire} />
-      <CustomButton text={`Toggle (${toggle})`} onClick={handleToggle} />
+      <CustomButton
+        text="Increment"
+        onClick={() => dispatch({ type: "IncrementMoney", payload: 1 })}
+      />
+      <CustomButton
+        text="Decrement"
+        onClick={() => dispatch({ type: "DecrementMoney", payload: 1 })}
+      />
+      <CustomButton
+        text={`Hire Farmer (${farmer})`}
+        onClick={() => dispatch({ type: "HireWorker", payload: "farmer" })}
+      />
+      <CustomButton
+        text={`Hire Miner (${miner})`}
+        onClick={() => dispatch({ type: "HireWorker", payload: "miner" })}
+      />
+      <CustomButton
+        text={`Toggle (${toggle})`}
+        onClick={handleToggle}
+      />
     </div>
   );
 };
 
 const CustomButton = ({ text, onClick }) => (
-  <button
-    className="btn"
-    type="button"
-    onClick={onClick}
-  >
-    {text}
-  </button>
+  <button className="btn" type="button" onClick={onClick}>{text}</button>
 );
 
 export default App;
